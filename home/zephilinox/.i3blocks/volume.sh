1c3bf70d6f4bbc7b56f3dc2096ed3ef2a2fc7e8e
Zephilinox <Zephilinox@hotmail.co.uk>
2014-11-27 07:05:24 +0000

    2014-11-27 @ 07:05:24

--- home/zephilinox/.i3blocks/volume.sh
+++ home/zephilinox/.i3blocks/volume.sh
@@ -1,8 +1,23 @@
+#Get the master output, see if muted. [on] or [off]
 VAL="$( amixer get Master | grep -F "Front Left:" | cut -d ' ' -f 9)"
 
 if [ "$VAL" == "[on]" ]
 then
-	VAL='['$(mpc volume | cut -d ':' -f 2 | cut -d ' ' -f 2)']'
+	PAUSED="$( mpc | grep -F "[paused]" )"
+        
+	if [ "$PAUSED" != "" ] #empty if not paused
+	then
+		VAL="[paused"]
+	else
+		VAL='['$(mpc volume | cut -d ':' -f 2 | cut -d ' ' -f 2)']'
+
+		if [ "$VAL" == "[]" ] #vol under 10, so different cut pos
+		then
+			VAL='['$(mpc volume | cut -d ':' -f 2 | cut -d ' ' -f 3)']'
+		fi
+	fi
+else
+	VAL="[muted]"
 fi
 
 echo $VAL
